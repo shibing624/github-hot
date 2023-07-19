bd3986680c39d706fc963a5c906fd5cc85d307a2
shibing624 <shibing624@126.com>
2023-07-19 21:26:10 +0800

    update schedule.

---
+++ crawler.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+import datetime
+import os
+from codecs import open
+
+import requests
+from pyquery import PyQuery
+
+
+def git_add_commit_push(date, filename):
+    cmd_git_add = 'git add {filename}'.format(filename=filename)
+    cmd_git_commit = 'git commit -m "{date}"'.format(date=date)
+    cmd_git_push = 'git push -u origin master'
+
+    os.system(cmd_git_add)
+    os.system(cmd_git_commit)
+    os.system(cmd_git_push)
+
+
+def create_markdown(date, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write("## " + date + "\n")
+
+
+def scrape(language, filename):
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip,deflate,sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8'
+    }
+
+    url = 'https://github.com/trending/{language}'.format(language=language)
+    r = requests.get(url, headers=HEADERS)
+    assert r.status_code == 200
+
+    d = PyQuery(r.content)
+    items = d('div.Box article.Box-row')
+
+    with open(filename, "a", "utf-8") as f:
+        f.write('\n#### {language}\n'.format(language=language))
+
+        for item in items:
+            i = PyQuery(item)
+            title = i(".lh-condensed a").text()
+            description = i("p.col-9").text()
+            url = i(".lh-condensed a").attr("href")
+            url = "https://github.com" + url
+            f.write(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
+
+
+def job():
+    today_str = datetime.datetime.now().strftime('%Y-%m-%d')
+    filename = 'markdowns/{date}.md'.format(date=today_str)
+
+    # create markdown file
+    create_markdown(today_str, filename)
+
+    # write markdown
+    scrape('', filename)  # full_url = 'https://github.com/trending?since=daily'
+    scrape('python', filename)
+    scrape('swift', filename)
+    scrape('javascript', filename)
+    scrape('go', filename)
+    print('save markdown file to {filename}'.format(filename=filename))
+
+    # git_add_commit_push(strdate, filename)
+
+
+if __name__ == '__main__':
+    job()
